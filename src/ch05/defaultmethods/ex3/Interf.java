f42c2a76a89c2d42d18f76e6e5488091b64f771f
Carlos Villanueva <ceva_19@hotmail.com>
2019-02-04 12:36:06 -0300

    Java 8 new Features - Defaults Methods & Static Method with Interface

---
+++ src/ch05/defaultmethods/ex3/Interf.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch05.defaultmethods.ex3;
+
+/**
+ *
+ * @author PC
+ */
+public interface Interf {
+    public static void m1()
+    {
+        System.out.println("Interface with static method");
+    }
+}
---
+++ src/ch05/defaultmethods/ex3/InterfaceStaticTest.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch05.defaultmethods.ex3;
+
+/**
+ *
+ * @author PC
+ */
+public class InterfaceStaticTest {
+    public static void main(String[] args) {
+        Interf.m1();
+    }
+}
