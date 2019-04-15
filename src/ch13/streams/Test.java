a70ea59faee3b78d2f3261e433a9b2d27b2bb8ff
Carlos Villanueva <ceva_19@hotmail.com>
2019-04-15 12:17:18 -0300

    Java 8 New Features - Chapter 13 - Streams

---
+++ src/ch13/streams/Test.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch13.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author PC
+ */
+public class Test {
+    
+    private static ArrayList<Integer> l = new ArrayList<Integer>();
+    
+    private static void populate()
+    {
+        l.add(0); l.add(5); l.add(10); l.add(15); l.add(20); l.add(25);
+    }
+    
+    private static void processCollectionWithOutStreams()
+    {
+        populate();
+        List<Integer> l1 = new ArrayList<Integer>();
+        for(Integer i1 : l)
+        {
+            if(i1 % 2 == 0)
+            {
+                l1.add(i1);
+            }
+        }
+        System.out.println("Collection without streams" + l1);
+    }
+    
+    private static void mappingCollectionStream()
+    {
+        List<Integer> l3 = l.stream().map(i -> i * 2).collect(Collectors.toList());
+        System.out.println("Stream Map Collection: " + l3);
+    }
+    
+    private static void processCollectionWithStreams()
+    {
+        List<Integer> l2 = l.stream()
+                .filter(i -> i % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println("Collection using streams: " + l2);
+    }
+    
+    public static void main(String[] args) {
+        processCollectionWithOutStreams();
+        processCollectionWithStreams();
+        mappingCollectionStream();
+    }
+}
